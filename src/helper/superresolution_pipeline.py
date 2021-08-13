f4c4a8d8783201891428c15be86e4a4b53580940
Paul Rötzer <paul@Pauls-MacBook-Pro-2.local>
2021-08-13 13:29:38 +0200

    Add function convert_to_grayscale

--- src/helper/superresolution_pipeline.py
+++ src/helper/superresolution_pipeline.py
@@ -16,3 +16,18 @@ def read_images(filenames):
     imgs.append(img)
 
   return imgs
+
+
+def convert_to_grayscale(imgs):
+  '''
+    input:
+      imgs:       list of cv2 images
+    output:
+      imgs_gray:  list of grayscale cv2 images
+  '''
+  imgs_gray = []
+  for img in imgs:
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgs_gray.append(img_gray)
+
+  return imgs_gray
