06055c9c886455fe61c09083754e1d308788a420
Paul Rötzer <paul@Pauls-MacBook-Pro-2.local>
2021-08-13 14:15:07 +0200

    Convert indentation to tabs

--- src/helper/plot.py
+++ src/helper/plot.py
@@ -2,31 +2,31 @@ from matplotlib import pyplot as plt
 import cv2
 
 def plot_image(img, title='', fig_size=[5, 3]):
-  plt.figure(figsize=fig_size)
-  plt.title(title, fontsize=12)
-  plt.axis('off')
-  plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+	plt.figure(figsize=fig_size)
+	plt.title(title, fontsize=12)
+	plt.axis('off')
+	plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 def plot_matches(img1, keypoints1, img2, keypoints2, matches, fig_size=[15, 12]):
-  matches_img = cv2.drawMatches(img1, keypoints1, img2, keypoints2, matches, img2, flags=2)
-  plot_image(matched_img, title='Related keypoints', fig_size=fig_size)
+	matches_img = cv2.drawMatches(img1, keypoints1, img2, keypoints2, matches, img2, flags=2)
+	plot_image(matched_img, title='Related keypoints', fig_size=fig_size)
 
 def plot_image_with_keypoints(img, keypoints, title='', fig_size=[40, 35]):
-  plt.figure(figsize=fig_size)
-  plt.title(title, fontsize=12)
-  img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  img = cv2.drawKeypoints(img_gray, keypoints, img)
-  plt.axis('off')
-  plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+	plt.figure(figsize=fig_size)
+	plt.title(title, fontsize=12)
+	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	img = cv2.drawKeypoints(img_gray, keypoints, img)
+	plt.axis('off')
+	plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 def plot_images(imgs, title='Images', fig_size=[20, 15]):
-  plt.figure(figsize=fig_size)
-  plt.title(title, fontsize=12)
-  num_images = len(imgs)
-  num_rows = math.ceil(num_images / 4)
-  img_index = 1
-  for img in imgs:
-    plt.subplot(num_rows, 4, img_index)
-    img_index = img_index + 1
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.axis('off')
+	plt.figure(figsize=fig_size)
+	plt.title(title, fontsize=12)
+	num_images = len(imgs)
+	num_rows = math.ceil(num_images / 4)
+	img_index = 1
+	for img in imgs:
+		plt.subplot(num_rows, 4, img_index)
+		img_index = img_index + 1
+		plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+		plt.axis('off')
--- src/helper/superresolution_pipeline.py
+++ src/helper/superresolution_pipeline.py
@@ -4,226 +4,226 @@ from PIL import Image
 from .plot import *
 
 def read_images(filenames):
-  '''
-    input:
-      filenames:  list of strings
-    output:
-      imgs:       list of cv2 images
-  '''
-  imgs = []
-  for filename in filenames:
-    img = cv2.imread(filename)
-    imgs.append(img)
+	'''
+		input:
+			filenames:  list of strings
+		output:
+			imgs:       list of cv2 images
+	'''
+	imgs = []
+	for filename in filenames:
+		img = cv2.imread(filename)
+		imgs.append(img)
 
-  return imgs
+	return imgs
 
 
 def convert_to_grayscale(imgs):
-  '''
-    input:
-      imgs:       list of cv2 images
-    output:
-      imgs_gray:  list of grayscale cv2 images
-  '''
-  imgs_gray = []
-  for img in imgs:
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgs_gray.append(img_gray)
+	'''
+		input:
+			imgs:       list of cv2 images
+		output:
+			imgs_gray:  list of grayscale cv2 images
+	'''
+	imgs_gray = []
+	for img in imgs:
+		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		imgs_gray.append(img_gray)
 
-  return imgs_gray
+	return imgs_gray
 
 
 def extract_features(imgs, num_features=100):
-  '''
-    function which extracts the SIFT features of the images
+	'''
+		function which extracts the SIFT features of the images
 
-    input:
-      imgs:         list of cv2 grayscale images
-      num_features: number of features to extract
-    output:
-      (keypoints, descriptor):  tuple containing list of
-                                keypoints and list of descriptors
-  '''
+		input:
+			imgs:         list of cv2 grayscale images
+			num_features: number of features to extract
+		output:
+			(keypoints, descriptor):  tuple containing list of
+																keypoints and list of descriptors
+	'''
 
-  # create SIFT feature extractor
-  sift = cv2.xfeatures2d.SIFT_create(nfeatures=num_features)
+	# create SIFT feature extractor
+	sift = cv2.xfeatures2d.SIFT_create(nfeatures=num_features)
 
-  all_keypoints = []
-  all_descriptors = []
+	all_keypoints = []
+	all_descriptors = []
 
-  for img in imgs:
-    # detect features from the image
-    keypoints, descriptors = sift.detectAndCompute(img, None)
+	for img in imgs:
+		# detect features from the image
+		keypoints, descriptors = sift.detectAndCompute(img, None)
 
-    all_keypoints.append(keypoints)
-    all_descriptors.append(descriptors)
+		all_keypoints.append(keypoints)
+		all_descriptors.append(descriptors)
 
-  return (all_keypoints, all_descriptors)
+	return (all_keypoints, all_descriptors)
 
 
 def match_descriptors_to_first_image(descriptors, num_matches=50):
-  '''
-    function which matches all keypoints of all images to the first image
+	'''
+		function which matches all keypoints of all images to the first image
 
-    input:
-      descriptors: list of descriptors
-      num_matches: number of best matches to keep after matching
-    output:
-      matches:  list of matches between img1 and other images
-  '''
+		input:
+			descriptors: list of descriptors
+			num_matches: number of best matches to keep after matching
+		output:
+			matches:  list of matches between img1 and other images
+	'''
 
-  # create feature matcher
-  bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+	# create feature matcher
+	bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
 
-  descriptor1 = descriptors[0]
-  matches = []
+	descriptor1 = descriptors[0]
+	matches = []
 
-  for descriptor in descriptors[1:]:
-    # match descriptors of both images
-    match = bf.match(descriptor1, descriptor)
-    # sort matches according to their distance and keep only num_matches matches
-    match = sorted(match, key = lambda x:x.distance)
-    matches.append(match[:num_matches])
+	for descriptor in descriptors[1:]:
+		# match descriptors of both images
+		match = bf.match(descriptor1, descriptor)
+		# sort matches according to their distance and keep only num_matches matches
+		match = sorted(match, key = lambda x:x.distance)
+		matches.append(match[:num_matches])
 
-  return matches
+	return matches
 
 
 def warp_images_to_first_image(matches, keypoints, imgs):
-    '''
-    function which warps all other images onto the first image
+	'''
+		function which warps all other images onto the first image
 
-    input:
-      matches:    list of matches between img1 and other images
-      keypoints:  keypoints of imgs
-      imgs:       rgb images
-    output:
-      aligned_images:  list of images which are warped onto the first image
-  '''
+		input:
+			matches:    list of matches between img1 and other images
+			keypoints:  keypoints of imgs
+			imgs:       rgb images
+		output:
+			aligned_images:  list of images which are warped onto the first image
+	'''
 
-  keypoints_img1 = keypoints[0]
-  img_src = imgs[0]
-  size_src = (img_src.shape[1], img_src.shape[0])
+	keypoints_img1 = keypoints[0]
+	img_src = imgs[0]
+	size_src = (img_src.shape[1], img_src.shape[0])
 
-  # first aligned image to first image is first image itself
-  aligned_images = [img_src]
+	# first aligned image to first image is first image itself
+	aligned_images = [img_src]
 
-  img_counter = 1
+	img_counter = 1
 
-  for matches_of_img in matches:
-    # extract keypoints of other image
-    assert img_counter < len(keypoints)
-    keypoints_other_img = keypoints[img_counter]
-    img_src = imgs[img_counter]
-    img_counter = img_counter + 1
+	for matches_of_img in matches:
+		# extract keypoints of other image
+		assert img_counter < len(keypoints)
+		keypoints_other_img = keypoints[img_counter]
+		img_src = imgs[img_counter]
+		img_counter = img_counter + 1
 
-    # reorder keypoints according to the matches and
-    # define src and destination keypoints for homography matrix
-    keypoints_src = []
-    keypoints_dest = []
-    for match in matches_of_img:
-      keypoints_src.append(keypoints_other_img[match.trainIdx].pt)
-      keypoints_dest.append(keypoints_img1[match.queryIdx].pt)
+		# reorder keypoints according to the matches and
+		# define src and destination keypoints for homography matrix
+		keypoints_src = []
+		keypoints_dest = []
+		for match in matches_of_img:
+			keypoints_src.append(keypoints_other_img[match.trainIdx].pt)
+			keypoints_dest.append(keypoints_img1[match.queryIdx].pt)
 
-    # convert to np array
-    keypoints_src = np.asarray(keypoints_src)
-    keypoints_dest = np.asarray(keypoints_dest)
+		# convert to np array
+		keypoints_src = np.asarray(keypoints_src)
+		keypoints_dest = np.asarray(keypoints_dest)
 
-    # compute homography matrix
-    h, status = cv2.findHomography(keypoints_src, keypoints_dest)
+		# compute homography matrix
+		h, status = cv2.findHomography(keypoints_src, keypoints_dest)
 
-    # src is beeing warped onto destination
-    # => src is other image, destination is first image
-    # img_dest is warped img_src
-    img_dst = cv2.warpPerspective(img_src, h, size_src)
+		# src is beeing warped onto destination
+		# => src is other image, destination is first image
+		# img_dest is warped img_src
+		img_dst = cv2.warpPerspective(img_src, h, size_src)
 
-    aligned_images.append(img_dst)
+		aligned_images.append(img_dst)
 
-  return aligned_images
+	return aligned_images
 
 
 def resize_img(img, scale_factor=1.0):
-  '''
-    function to resize an image
-    -> scale_factor > 1 increase the size
-    -> scale_factor < 1 descrease the size
+	'''
+		function to resize an image
+		-> scale_factor > 1 increase the size
+		-> scale_factor < 1 descrease the size
 
-    input:
-      img:           rgb image
-      scale_factor:  factor with which the resize should happen
-    output:
-      resized_img:   bigger or smaller image
-  '''
-  width  = int(img.shape[1] * scale_factor)
-  height = int(img.shape[0] * scale_factor)
+		input:
+			img:           rgb image
+			scale_factor:  factor with which the resize should happen
+		output:
+			resized_img:   bigger or smaller image
+	'''
+	width  = int(img.shape[1] * scale_factor)
+	height = int(img.shape[0] * scale_factor)
 
-  new_size = (width, height)
+	new_size = (width, height)
 
-  return cv2.resize(img, new_size)
+	return cv2.resize(img, new_size)
 
 
 def compute_average_image(imgs, algorithm='median'):
-  '''
-    function to compute average img of a list of imgs
+	'''
+		function to compute average img of a list of imgs
 
-    input:
-      imgs:       list of rgb image
-      algorithm:  'mean' or 'median' => median reproduces sharper imgs
-    output:
-      avg_img:    average image
-  '''
-  imgs = np.asarray(imgs)
-  if algorithm == 'median':
-    avg_img = np.median(imgs, axis=0)
-  elif algorithm == 'mean':
-    avg_img = np.mean(imgs, axis=0)
-  else:
-    raise NameError('Algorithm " %s " not supported' % algorithm)
+		input:
+			imgs:       list of rgb image
+			algorithm:  'mean' or 'median' => median reproduces sharper imgs
+		output:
+			avg_img:    average image
+	'''
+	imgs = np.asarray(imgs)
+	if algorithm == 'median':
+		avg_img = np.median(imgs, axis=0)
+	elif algorithm == 'mean':
+		avg_img = np.mean(imgs, axis=0)
+	else:
+		raise NameError('Algorithm " %s " not supported' % algorithm)
 
-  # convert to image again
-  avg_img = Image.fromarray(avg_img.astype(np.uint8))
+	# convert to image again
+	avg_img = Image.fromarray(avg_img.astype(np.uint8))
 
-  return avg_img
+	return avg_img
 
 
 def superresolution_pipeline(filenames,
-                              output_filename='superresmean.jpg',
-                              resize_scale=5,
-                              num_features=50,
-                              verbose=False):
-  '''
-    function which computes a superresolution image out of a list of
-    unaligned images of the sane object
+															output_filename='superresmean.jpg',
+															resize_scale=5,
+															num_features=50,
+															verbose=False):
+	'''
+		function which computes a superresolution image out of a list of
+		unaligned images of the sane object
 
-    input:
-      filenames:        list of filenames
-      output_filename:  filename of superresolution img
-      resize_scale:     amount by which the imgs should be enlarged
-      num_features:     number of features which are used for alignement
-      verbose:          if some plots should be generated or not
-    output:
-      superres_img:     superresolution image
-  '''
-  imgs = read_images(filenames)
+		input:
+			filenames:        list of filenames
+			output_filename:  filename of superresolution img
+			resize_scale:     amount by which the imgs should be enlarged
+			num_features:     number of features which are used for alignement
+			verbose:          if some plots should be generated or not
+		output:
+			superres_img:     superresolution image
+	'''
+	imgs = read_images(filenames)
 
-  if verbose:
-    plot_images(imgs, title='Input Images')
+	if verbose:
+		plot_images(imgs, title='Input Images')
 
-  # resize images
-  for i in range(0, len(imgs)):
-    imgs[i] = resize_img(imgs[i], scale_factor=resize_scale)
+	# resize images
+	for i in range(0, len(imgs)):
+		imgs[i] = resize_img(imgs[i], scale_factor=resize_scale)
 
-  imgs_gray = convert_to_grayscale(imgs)
+	imgs_gray = convert_to_grayscale(imgs)
 
-  keypoints, descriptors = extract_features(imgs_gray)
+	keypoints, descriptors = extract_features(imgs_gray)
 
-  matches = match_descriptors_to_first_image(descriptors, num_keypoints)
+	matches = match_descriptors_to_first_image(descriptors, num_keypoints)
 
-  aligned_images = warp_images_to_first_image(matches, keypoints, imgs)
+	aligned_images = warp_images_to_first_image(matches, keypoints, imgs)
 
-  superres_img = compute_average_image(aligned_images)
-  if verbose
-    plot_image(superres_img, title='Superres Image')
+	superres_img = compute_average_image(aligned_images)
+	if verbose:
+		plot_image(superres_img, title='Superres Image')
 
-  cv2.imwrite(output_filename, np.asarray(superres_img))
+	cv2.imwrite(output_filename, np.asarray(superres_img))
 
-  return superres_img
+	return superres_img
