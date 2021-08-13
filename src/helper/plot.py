64b9a1050d0114099c312ad81c6cb513cfc815ac
Paul Rötzer <paul@Pauls-MacBook-Pro-2.local>
2021-08-13 13:37:48 +0200

    Fix color issue with plots
    CV2 somehow uses bgr instead of rgb format so we have to
    convert the images before plotting.

--- src/helper/plot.py
+++ src/helper/plot.py
@@ -5,7 +5,7 @@ def plot_image(img, title='', fig_size=[5, 3]):
   plt.figure(figsize=fig_size)
   plt.title(title, fontsize=12)
   plt.axis('off')
-  plt.imshow(img)
+  plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 def plot_matches(img1, keypoints1, img2, keypoints2, matches, fig_size=[15, 12]):
   matches_img = cv2.drawMatches(img1, keypoints1, img2, keypoints2, matches, img2, flags=2)
@@ -17,7 +17,7 @@ def plot_image_with_keypoints(img, keypoints, title='', fig_size=[40, 35]):
   img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   img = cv2.drawKeypoints(img_gray, keypoints, img)
   plt.axis('off')
-  plt.imshow(img)
+  plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 def plot_images(imgs, title='Images', fig_size=[20, 15]):
   plt.figure(figsize=fig_size)
@@ -28,5 +28,5 @@ def plot_images(imgs, title='Images', fig_size=[20, 15]):
   for img in imgs:
     plt.subplot(num_rows, 4, img_index)
     img_index = img_index + 1
-    plt.imshow(img)
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     plt.axis('off')
