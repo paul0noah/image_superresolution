04b81994b36523fca7e06be30b35db424d26cbf7
Paul Rötzer <paul@Pauls-MacBook-Pro-2.local>
2021-08-13 13:35:44 +0200

    Add function read images

--- src/helper/superresolution_pipeline.py
+++ src/helper/superresolution_pipeline.py
@@ -2,3 +2,17 @@ import cv2
 import numpy as np
 from PIL import Image
 from .plot import *
+
+def read_images(filenames):
+  '''
+    input:
+      filenames:  list of strings
+    output:
+      imgs:       list of cv2 images
+  '''
+  imgs = []
+  for filename in filenames:
+    img = cv2.imread(filename)
+    imgs.append(img)
+
+  return imgs
